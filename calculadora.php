b0c3d900dfff3d61f914d315f9b614bf8db0b8cf
nerigleston <109543650+nerigleston@users.noreply.github.com>
2023-08-31 10:47:31 -0300

    Add files via upload

---
+++ calculadora.php
@@ -0,0 +1,12 @@
+<?php
+$numero1 = 40;
+$numero2 = 20;
+$soma = $numero1 + $numero2;
+$sub = $numero1 - $numero2;
+$multi = $numero1 * $numero2;
+$div = $numero1 / $numero2;
+echo "Soma = $soma;" .PHP_EOL;
+echo "Subtração = $sub;" .PHP_EOL;
+echo "Multiplicação = $multi;" .PHP_EOL;
+echo "Divisão = $div;" .PHP_EOL;
+?>
